247e4ef0d39d4302c20ba329c72794e70fa83014
Aathithya P R <ironaathithya@gmail.com>
2025-06-10 22:32:18 +0530

    new

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -58,8 +58,8 @@ const Header = () => {
   }, [isMenuOpen]);
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-transparent">
-      <div className="w-full backdrop-blur-lg bg-transparent border-b border-white/10 transition-all duration-300">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-gray-900">
+      <div className="w-full border-b border-white/10 transition-all duration-300">
         <div className="container mx-auto px-4 md:px-10 h-16 flex items-center justify-between">
           {/* Logo left */}
           <a
